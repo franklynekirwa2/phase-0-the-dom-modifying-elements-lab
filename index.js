583ae6d09be779f1d44b8c71077230bc3db16cf7
franklynekirwa2 <sangkirwa2@gmail.com>
2024-07-09 15:03:41 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your code here!
+const btn = document.querySelector('.btn')
+const newHeader=document.createElement("h1");
+newHeader.textContent="YOUR-NAME is the champion";
+newHeader.id="victory";
+
+btn.addEventListener('click', () => {
+  const title = document.querySelector('.title')
+  title.remove()
+})
+
+const list_items = document.querySelectorAll('.list-item')
+console.log(list_items)
+
+const spaned = document.getElementById('some-text')
+console.log(spaned)
+
+const list_item_three = document.querySelector('.list-item:nth-child(3)')
+list_item_three.innerText = 'This item has been changed!'
